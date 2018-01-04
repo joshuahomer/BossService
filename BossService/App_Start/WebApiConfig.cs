49f2e56c1c3c6822ae49a2f2420e88d92191ab20
Joshua Homer <joshuahomer@msn.com>
2018-01-04 09:35:45 -0700

    Added timer so that the server will tweet a random sentence every so often.

--- BossService/App_Start/WebApiConfig.cs
+++ BossService/App_Start/WebApiConfig.cs
@@ -1,16 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web.Http;
+using BossService.Controllers;
 
 namespace BossService
 {
     public static class WebApiConfig
     {
+        static TwitterController twitter;
+        //https://msdn.microsoft.com/en-us/library/system.threading.timer(v=vs.110).aspx
+        //https://social.msdn.microsoft.com/Forums/en-US/48f77cfb-b8f7-46a5-9de9-6f681b3a6373/calling-method-every-hour?forum=csharpgeneral
+        private static void PrintMessage(Object state)
+        {
+            twitter.Get(245687543);
+        }
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
+            twitter = new TwitterController();
+            //automatically tweet 3 times a day - 28800000
+            Timer timer = new Timer(PrintMessage, null, 0, 3600000);
             // Web API routes
             config.MapHttpAttributeRoutes();
 
